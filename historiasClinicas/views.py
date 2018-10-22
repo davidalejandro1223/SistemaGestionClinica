b9d81d05cd5f46872ee908986a6153ce57911fe2
Jairo A Romero <jaaromerot@correo.udistrital.edu.co>
2018-10-22 16:12:38 -0500

    Cambios en el pdf

--- historiasClinicas/views.py
+++ historiasClinicas/views.py
@@ -20,8 +20,10 @@ from reportlab.lib.enums import TA_CENTER
 from reportlab.lib import colors
 from django.http import HttpResponse
 from reportlab.lib.utils import ImageReader
-from reportlab.lib.units import inch
+from reportlab.lib.units import cm
 from reportlab.lib.colors import black
+from reportlab.pdfbase.pdfmetrics import stringWidth
+from reportlab.rl_config import defaultPageSize
 
 
 # Create your views here.
@@ -83,12 +85,15 @@ class HistoriaClinicaListView(ListView):
     model = Cabecera
 
 def report(request, pk):
-    margenIzq=30;
+    
+    PAGE_WIDTH  = defaultPageSize[0]
+
+    margenIzq=30
     response = HttpResponse(content_type='applicatio/pdf')
     response['content-Disposition'] = 'attachment; filename= historia.pdf'
     buffer=BytesIO()
     c = canvas.Canvas(buffer, pagesize=A4)
-    
+
     #HEADER
     c.setLineWidth(.3)
     c.setFont('Helvetica-Bold', 14)
@@ -96,8 +101,15 @@ def report(request, pk):
     c.setFont('Helvetica', 11)
     c.drawString(margenIzq+285,768, 'Consultorios ocupacionales y de medicina general.')
     c.drawString(margenIzq+324,756, 'Calle 4 #4 -97  Facatativá (Cundinamarca).')
+    #c.setFont('Helvetica-Bold', 11)
+    #text= 'EXAMENES REALIZADOS'
+    #text_width = stringWidth(text,'Helvetica-Bold',11 )
+    #c.line(margenIzq, 500, 560,500)
+    #c.drawString((PAGE_WIDTH - text_width) / 2.0, 505, text)
+    text='CERTIFICADO DE APTITUD LABORAL'
+    text_width = stringWidth(text,'Helvetica-Bold',14)
     c.setFont('Helvetica-Bold', 14)
-    c.drawString(173,725, 'CERTIFICADO DE APTITUD LABORAL')
+    c.drawString((PAGE_WIDTH - text_width) / 2.0,725, 'CERTIFICADO DE APTITUD LABORAL')
 
     #LOGOTIPO
     logo=os.path.join(os.path.dirname(os.path.abspath(__file__)), './Imagenes/logo.png')
@@ -128,7 +140,8 @@ def report(request, pk):
 
     c.setFont('Helvetica', 9)
     c.drawString(margenIzq+63,648,paciente.cedula)
-    c.drawString(75,668,paciente.primer_nombre+' '+paciente.segundo_nombre+' '+paciente.primer_apellido+' '+paciente.segundo_apellido)
+    c.drawString(75,668,paciente.primer_nombre+' '+paciente.segundo_nombre+' '
+        +paciente.primer_apellido+' '+paciente.segundo_apellido)
     #c.drawString(margenIzq+190, 648, paciente.edad)
     c.drawString(margenIzq+292,648, paciente.sexo)
     #c.drawString(margenIzq+90, 628, 'F.Nacimiento')
@@ -137,8 +150,87 @@ def report(request, pk):
     #c.drawString(margenIzq+50, 568, 'empresa')
     #c.drawString(margenIzq+50, 548, 'cargo')
 
+    #Tabla examenes
+    #   Header
+    styles = getSampleStyleSheet()
+    styleBH= styles["Normal"]
+    styleBH.alignment = TA_CENTER
+    styleBH.fontsize = 10
+    styleBH.fontName = 'Helvetica-Bold'
+
+    examenes=Paragraph('''EXAMENES REALIZADOS''', styleBH)
+    dataTabla=[]
+    dataTabla.append([examenes])
+
+    #   Contenido
+    styleN=styles["BodyText"]
+    styleN.fontSize = 9
+    styleN.fontName = 'Helvetica'
+
+    high = 480
+    cadena=[Paragraph('''Examen medico ocupacional básico.''', styleN)]
+    dataTabla.append(cadena)
+
+    width, height = A4
+    tabla = Table(dataTabla, colWidths=[19*cm, 9.5*cm])
+    tabla.setStyle(TableStyle([
+        ('INNERGRID', (0,0), (-1,-1), 1, colors.black),
+        ('BOX', (0,0), (-1,-1), 1, colors.black),]))
+    #c.drawString(margenIzq, 490, 'Examen medico ocupacional básico.')
+    
+    tabla.wrapOn(c,width, height)
+    tabla.drawOn(c, margenIzq-2, high)
+    c.showPage
+
+
+    #Tabla Concepto
+    #   Header
+    concepto=Paragraph('''CONCEPTO''', styleBH)
+    dataTablaConcepto=[]
+    dataTablaConcepto.append([concepto])
+    
+
+    #   Contenido
+    #high = 415
+    #cadena=[Paragraph('''A''', styleN)]
+    #dataTablaConcepto.append(cadena)
+
+    width, height = A4
+    tablaConcepto = Table(dataTablaConcepto, colWidths=[19*cm, 9.5*cm])
+    tablaConcepto.setStyle(TableStyle([
+        ('INNERGRID', (0,0), (-1,-1), 1, colors.black),
+        ('BOX', (0,0), (-1,-1), 1, colors.black),]))
+    #c.drawString(margenIzq, 490, 'Examen medico ocupacional básico.')
+    
+    tablaConcepto.wrapOn(c,width, height)
+    tablaConcepto.drawOn(c, margenIzq-2, high)
+    c.showPage
+
+
+    #Tabla observaciones
+    #   Header
+    observacion=Paragraph('''OBSERVACIONES''', styleBH)
+    dataTablaObserv=[]
+    dataTablaObserv.append([observacion])
+
+    #   Contenido
+    high = 50
+    cadena=[Paragraph('''A''', styleN)]
+    dataTablaObserv.append(cadena)
+
+    width, height = A4
+    tablaObserv = Table(dataTablaObserv, colWidths=[19*cm, 9.5*cm], rowHeights=(5*cm, 5*cm))
+    tablaObserv.setStyle(TableStyle([
+        ('INNERGRID', (0,0), (-1,-1), 1, colors.black),
+        ('BOX', (0,0), (-1,-1), 1, colors.black),]))
+    #c.drawString(margenIzq, 490, 'Examen medico ocupacional básico.')
+    
+    tablaObserv.wrapOn(c,width, height)
+    tablaObserv.drawOn(c, margenIzq-2, high)
+    c.showPage
+
     c.setStrokeColor(black)
-    c.setLineWidth(2)
+    c.setLineWidth(1)
     c.rect(margenIzq-2,540, 538,165, fill=0)
 
     
